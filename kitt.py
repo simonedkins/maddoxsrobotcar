923d9981c0098bb3d67224e1ad9799519989cd86
Simon Edkins <simon.edkins@yahoo.co.uk>
2013-06-24 21:54:30 +0000

    Of course a robot car should be able to talk

--- kitt.py
+++ kitt.py
@@ -2,18 +2,27 @@
 
 import piface.pfio as pfio
 from time import sleep
+from espeak import espeak
+
 
 pfio.init()
 
-while True:
-	for i in range(1,9):
-		pfio.digital_write(i,True)
-		sleep(0.2)
 
-	for i in range(1,9):
-		pfio.digital_write(i,False)
-		sleep(0.2)
-  
-  
+try:
+	while True:
+		espeak.synth("Turning the lights on")
+		for i in range(1,9):
+			pfio.digital_write(i,True)
+			sleep(0.2)
+
+		sleep(1)
+		espeak.synth("Turning the lights off")
+		for i in range(1,9):
+			pfio.digital_write(i,False)
+			sleep(0.2)
+		sleep(1)
+
+except KeyboardInterrupt:
+	print 'Bye...' 
 
 
