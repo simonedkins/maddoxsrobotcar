4ff4d7a9e73b684709be095333e5b097959226e5
Simon Edkins <simon.edkins@yahoo.co.uk>
2013-07-09 20:53:03 +0000

    added wheel class with forwards, backwards, stop and speed control

--- pwmface.py
+++ pwmface.py
@@ -7,9 +7,10 @@ import threading
 pfio.init()
 
 class Pwm(threading.Thread):
-	def __init__(self, pin=3, f=10.0, dc=0.5):
+	def __init__(self, pin, f, dc):
 		threading.Thread.__init__(self)
 		self._stop = threading.Event()
+		self._pause = threading.Event()
 		self.f = f
 		self.dc = dc
 		self.pin = pin
@@ -17,26 +18,90 @@ class Pwm(threading.Thread):
 	def stop(self):
 		self._stop.set()
 
+	def pause(self):
+		self._pause.set()
+
+	def unpause(self):
+		self._pause.clear()
+
+	def paused(self):
+		return self._pause.isSet()
+
 	def stopped(self):
 		return self._stop.isSet()
 
-	def set_dc(self,dc=0.5):
+	def set_dc(self,dc):
 		self.dc = dc
 	
-	def set_f(self,f=10.0):
+	def set_f(self,f):
 		self.f = f
 
 		
 	def run(self):
 		while not self.stopped():
-			total_time = 1/self.f
-			on_time = total_time * self.dc
-			off_time = total_time - on_time
-			pin = self.pin
-			pfio.digital_write(pin,True)
-			sleep(on_time)
-			pfio.digital_write(pin,False)
-			sleep(off_time)
+			if not self.paused():
+				total_time = 1/self.f
+				on_time = total_time * self.dc
+				off_time = total_time - on_time
+				pin = self.pin
+				pfio.digital_write(pin,True)
+				sleep(on_time)
+				pfio.digital_write(pin,False)
+				sleep(off_time)
+
+
+
+class Wheel():
+	def __init__(self, side, enable_pin, forwards_pin, backwards_pin):
+		self.side = side
+		self.enable_pin = enable_pin
+		self.forwards_pin = forwards_pin
+		self.backwards_pin = backwards_pin
+		self._speed = 0.5
+		self.f = 5.0
+		
+		pfio.digital_write(self.enable_pin, True)
+	
+	def forwards(self, speed):
+		self.stop()
+		self._speed = speed
+		pfio.digital_write(self.enable_pin, True)
+		self.d = Pwm(pin=self.forwards_pin, f=self.f, dc=self._speed)	
+		self.d.start()
+
+	def backwards(self, speed):
+		self.stop()
+		self._speed = speed
+		pfio.digital_write(self.enable_pin, True)
+		self.d = Pwm(pin=self.backwards_pin, f=self.f, dc=self._speed)	
+		self.d.start()
+
+	def speed(self, speed):
+		self._speed = speed
+		self.d.set_dc(self._speed)	
+
+
+	def stop(self):
+		try:	
+			self.d.stop()
+			self.d = None
+		except AttributeError:
+			pass
+	
+		pfio.digital_write(self.enable_pin, False)
+		pfio.digital_write(self.forwards_pin, False)
+		pfio.digital_write(self.backwards_pin, False)
+
+
+
+	
+
+
+
+
+
+
+
 
 
 
@@ -49,24 +114,22 @@ class Pwm(threading.Thread):
 if __name__ == '__main__':
 
 
-	drive = Drive()
-	drive.start()
+	x = Wheel('left',1,3,7)
+	y = Wheel('right',2,4,8)
+	x.forwards(1.0)
+	y.forwards(0.8)
 	sleep(5)
-
-	drive.stop()
-
-
-
-
-	print 'ended exec'
-	drive.join()
-	print 'ended program'
-
-
-
-
-
-
+	x.speed(0.2)
+	y.speed(0.5)
+	sleep(5)
+	x.backwards(0.8)
+	y.backwards(1.0)
+	sleep(5)
+	x.speed(1.0)
+	x.speed(0.5)
+	sleep(5)
+	x.stop()
+	y.stop()
 
 
 
